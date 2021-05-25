ea310c987a74f04e7afcd0fc2df196dfa0f388f8
but-sergey <but-sergey@bk.ru>
2021-05-25 21:20:06 +0300

    Сотрудники магазина

--- src/WebStore/WebStore/Controllers/Home.cs
+++ src/WebStore/WebStore/Controllers/Home.cs
@@ -4,12 +4,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStore.Models;
 
 namespace WebStore.Controllers
 {
     //[Controller]
     public class HomeController : Controller
     {
+        private static readonly List<Employee> __Employees = new()
+        {
+            new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 27 },
+            new Employee { Id = 2, LastName = "Петров", FirstName = "Петр", Patronymic = "Петрович", Age = 31 },
+            new Employee { Id = 3, LastName = "Сидоров", FirstName = "Сидор", Patronymic = "Сидорович", Age = 18 },
+        };
+
         private readonly IConfiguration _Configuration;
 
         public HomeController(IConfiguration Configuration)
@@ -24,7 +32,12 @@ namespace WebStore.Controllers
 
         public IActionResult SecondAction()
         {
-            return Content(_Configuration["Greetings"]);
+            return View("Index");
+        }
+
+        public IActionResult Employees()
+        {
+            return View(__Employees);
         }
     }
 }
