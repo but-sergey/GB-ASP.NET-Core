b1e6e08fcd597e77951ed36c0ea7e7ebe4f0cfab
but-sergey <but-sergey@bk.ru>
2021-06-01 09:37:39 +0300

    Добавление представлений в контроллер Home. Исправление ссылок на статические элементы

--- src/WebStore/WebStore/Controllers/Home.cs
+++ src/WebStore/WebStore/Controllers/Home.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using WebStore.Models;
 
 namespace WebStore.Controllers
@@ -25,10 +22,25 @@ namespace WebStore.Controllers
             _Configuration = Configuration;
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+        public IActionResult Index() => View();
+
+        public IActionResult Blog() => View();
+
+        public IActionResult BlogSingle() => View();
+
+        public IActionResult Cart() => View();
+
+        public IActionResult Checkout() => View();
+
+        public IActionResult ContactUs() => View();
+
+        public IActionResult Error404() => View();
+
+        public IActionResult Login() => View();
+
+        public IActionResult ProductDetails() => View();
+
+        public IActionResult Shop() => View();
 
         public IActionResult SecondAction()
         {
