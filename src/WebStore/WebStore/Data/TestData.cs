2c8ac549aad3c054b955165dac85847a26965137
but-sergey <but-sergey@bk.ru>
2021-06-08 13:34:26 +0300

    Перенос сотрудников в отдельный класс тестовых данных

---
+++ src/WebStore/WebStore/Data/TestData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using WebStore.Models;
+
+namespace WebStore.Data
+{
+    public static class TestData
+    {
+        public static List<Employee> Employees { get; } = new()
+        {
+            new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 27 },
+            new Employee { Id = 2, LastName = "Петров", FirstName = "Петр", Patronymic = "Петрович", Age = 31 },
+            new Employee { Id = 3, LastName = "Сидоров", FirstName = "Сидор", Patronymic = "Сидорович", Age = 18 },
+        };
+
+
+    }
+}
