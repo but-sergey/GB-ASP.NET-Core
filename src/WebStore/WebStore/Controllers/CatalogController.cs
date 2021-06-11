ac5a611d3cce7fc4020f7708cc73b173be0dfa66
but-sergey <but-sergey@bk.ru>
2021-06-11 14:25:11 +0300

    Контроллер каталока товаров

--- src/WebStore/WebStore/Controllers/CatalogController.cs
+++ src/WebStore/WebStore/Controllers/CatalogController.cs
@@ -3,19 +3,42 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStore.Domain;
 using WebStore.Services.Interfaces;
+using WebStore.ViewModels;
 
 namespace WebStore.Controllers
 {
     public class CatalogController : Controller
     {
-        private readonly IProductData productData;
+        private readonly IProductData _ProductData;
 
-        public CatalogController(IProductData ProductData) => productData = ProductData;
+        public CatalogController(IProductData ProductData) => _ProductData = ProductData;
 
         public IActionResult Index(int? BrandId, int? SectionId)
         {
-            return View();
+            var filter = new ProductFilter
+            {
+                BrandId = BrandId,
+                SectionId = SectionId,
+            };
+
+            var products = _ProductData.GetProducts(filter);
+
+            return View(new CatalogViewModel
+            { 
+                BrandId = BrandId,
+                SectionId = SectionId,
+                Products = products
+                .OrderBy(p => p.Order)
+                .Select(p => new ProductViewModel
+                {
+                    Id = p.Id,
+                    Name = p.Name,
+                    Price = p.Price,
+                    ImageUrl = p.ImageUrl,
+                })
+            });
         }
     }
 }
