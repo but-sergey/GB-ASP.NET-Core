b53d0510b16bbb9c3925218e2935e7ac4017b848
but-sergey <but-sergey@bk.ru>
2021-06-11 11:55:17 +0300

    Обучение сервиса каталога товаров работе с товарами

--- src/WebStore/WebStore/Services/InMemoryProductData.cs
+++ src/WebStore/WebStore/Services/InMemoryProductData.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebStore.Data;
+using WebStore.Domain;
 using WebStore.Domain.Entities;
 using WebStore.Services.Interfaces;
 
@@ -13,5 +14,20 @@ namespace WebStore.Services
         public IEnumerable<Section> GetSections() => TestData.Sections;
 
         public IEnumerable<Brand> GetBrands() => TestData.Brands;
+
+        public IEnumerable<Product> GetProducts(ProductFilter Filter = null)
+        {
+            IEnumerable<Product> query = TestData.Products;
+
+            //if (Filter?.SectionId != null)
+            //    query = query.Where(product => product.SectionId == Filter.SectionId);
+            if (Filter?.SectionId is { } section_id)
+                query = query.Where(product => product.SectionId == section_id);
+
+            if (Filter?.BrandId is { } brand_id)
+                query = query.Where(product => product.BrandId == brand_id);
+
+            return query;
+        }
     }
 }
