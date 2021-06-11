538a369462ff02018793351876609c8986b9857b
but-sergey <but-sergey@bk.ru>
2021-06-11 13:47:32 +0300

    Подготовка представления каталога товаров

---
+++ src/WebStore/WebStore/Controllers/CatalogController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Services.Interfaces;
+
+namespace WebStore.Controllers
+{
+    public class CatalogController : Controller
+    {
+        private readonly IProductData productData;
+
+        public CatalogController(IProductData ProductData) => productData = ProductData;
+
+        public IActionResult Index(int? BrandId, int? SectionId)
+        {
+            return View();
+        }
+    }
+}
