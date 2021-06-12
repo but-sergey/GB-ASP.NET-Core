90fed85361fa671ef09d1791fc77d78198621af4
but-sergey <but-sergey@bk.ru>
2021-06-12 15:14:47 +0300

    Подключение комтекста БД в основном приложении в качестве сервиса

--- src/WebStore/WebStore/Startup.cs
+++ src/WebStore/WebStore/Startup.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.IO;
+using WebStore.DAL.Context;
 using WebStore.Infrastructure.Conventions;
 using WebStore.Infrastructure.MiddleWare;
 using WebStore.Services;
@@ -24,6 +26,9 @@ namespace WebStore
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<WebStoreDB>(opt =>
+                opt.UseSqlServer(Configuration.GetConnectionString("MSSQL")));
+
             services.AddSingleton<IEmployeesData, InMemoryEmployeesData>();
 
             services.AddSingleton<IProductData, InMemoryProductData>();
