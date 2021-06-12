2c223d9aa9ff413ae920850e5d898ca28b4492ad
but-sergey <but-sergey@bk.ru>
2021-06-12 13:01:57 +0300

    Удаление хлама

--- src/WebStore/WebStore/Startup.cs
+++ src/WebStore/WebStore/Startup.cs
@@ -5,9 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
-using System.Diagnostics;
 using System.IO;
-using System.Threading.Tasks;
 using WebStore.Infrastructure.Conventions;
 using WebStore.Infrastructure.MiddleWare;
 using WebStore.Services;
@@ -26,17 +24,7 @@ namespace WebStore
 
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddScoped<ITestService, TestService>();
-            //services.AddScoped<IPrinter, DebugPrinter>();
-
-            services.AddSingleton<IEmployeesData, InMemoryEmployeesData>();   // Объект создается один раз на все время работы приложения
-                                                                              // нужен если сервис должен хранить состояние на время работы приложения
-
-            //services.AddScoped<IEmployeesData, InMemoryEmployeesData>();      // Объект создается на время жизни некоторой области
-            // если нужен сервис, который обладаем памятью только в пределах обработки одного запроса
-
-            //services.AddTransient<IEmployeesData, InMemoryEmployeesData>();   // Объект создается каждый раз заново
-            // когда сервис не подразумевает наличие внутренней памяти
+            services.AddSingleton<IEmployeesData, InMemoryEmployeesData>();
 
             services.AddSingleton<IProductData, InMemoryProductData>();
 
@@ -46,20 +34,6 @@ namespace WebStore
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider services)
         {
-            //var test_service = services.GetRequiredService<ITestService>();
-
-            //test_service.Test();
-
-            //var employees1 = services.GetService<IEmployeesData>();
-            //var employees2 = services.GetService<IEmployeesData>();
-
-            //IEmployeesData employees3;
-
-            //using (var scope = services.CreateScope())
-            //    employees3 = scope.ServiceProvider.GetService<IEmployeesData>();
-
-            //var is_equals = ReferenceEquals(employees1, employees2);
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -71,68 +45,19 @@ namespace WebStore
 
             app.UseMiddleware<TestMiddleWare>();
 
-            //app.Use(async (context, next) =>
-            //{
-            //    await next();
-            //});
-
-            //app.Map("/TestMapRequest", opt => opt.Run(async context =>
-            //{
-            //    await Task.Delay(100);
-            //    var stream_writer = new StreamWriter(context.Response.Body);
-            //    await stream_writer.WriteAsync("Hello from TestMapRequest");
-            //    await context.Response.CompleteAsync();
-            //}));
-
             app.UseWelcomePage("/WelcomePage");
 
-            //var greetings = Configuration["Greetings"];
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/greetings", async context =>
                 {
-                    //await context.Response.WriteAsync(greetings);
                     await context.Response.WriteAsync(Configuration["Greetings"]);
                 });
 
-                //endpoints.MapDefaultControllerRoute();
                 endpoints.MapControllerRoute(
                     "default",
                     "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
-
-    //interface ITestService
-    //{
-    //    void Test();
-    //}
-
-    //class TestService : ITestService
-    //{
-    //    private IPrinter _Printer;
-
-    //    public TestService(IPrinter Printer)
-    //    {
-    //        _Printer = Printer;
-    //    }
-
-    //    public void Test()
-    //    {
-    //        _Printer.Print("Запуск теста");
-    //    }
-    //}
-
-    //interface IPrinter
-    //{
-    //    void Print(string str);
-    //}
-
-    //class DebugPrinter : IPrinter
-    //{
-    //    public void Print(string str)
-    //    {
-    //        Debug.WriteLine(str);
-    //    }
-    //}
 }
