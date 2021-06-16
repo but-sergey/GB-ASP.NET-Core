596e3e3b2e246a6d0ddb66ff7f9f64fff7db51f0
but-sergey <but-sergey@bk.ru>
2021-06-16 08:40:33 +0300

    Инициализация БД данными для системы Identity

--- src/WebStore/WebStore/Data/WebStoreDBInitializer.cs
+++ src/WebStore/WebStore/Data/WebStoreDBInitializer.cs
@@ -1,19 +1,31 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using System;
 using System.Diagnostics;
 using System.Linq;
+using System.Threading.Tasks;
 using WebStore.DAL.Context;
+using WebStore.Domain.Entities.Identity;
 
 namespace WebStore.Data
 {
     public class WebStoreDBInitializer
     {
         private readonly WebStoreDB _db;
+        private readonly UserManager<User> _UserManager;
+        private readonly RoleManager<Role> _RoleManager;
         private readonly ILogger<WebStoreDBInitializer> _Logger;
 
-        public WebStoreDBInitializer(WebStoreDB db, ILogger<WebStoreDBInitializer> Logger)
+        public WebStoreDBInitializer(
+            WebStoreDB db,
+            UserManager<User> UserManager,
+            RoleManager<Role> RoleManager,
+            ILogger<WebStoreDBInitializer> Logger)
         {
             _db = db;
+            _UserManager = UserManager;
+            _RoleManager = RoleManager;
             _Logger = Logger;
         }
 
@@ -47,6 +59,19 @@ namespace WebStore.Data
                 _Logger.LogError("Ошибка при инициализации товаров в БД");
                 throw;
             }
+
+            try
+            {
+                //InitializeIdentityAsync().Wait();
+                InitializeIdentityAsync().GetAwaiter().GetResult();
+            }
+            catch
+            {
+                _Logger.LogError("Ошибка при инициализации данных Бд системы Identity");
+                throw;
+            }
+
+
 
             _Logger.LogInformation("Инициализация БД завершена за {0}с", timer.Elapsed.TotalSeconds);
         }
@@ -99,6 +124,56 @@ namespace WebStore.Data
             }
 
             _Logger.LogInformation("Инициализация товаров выполнена за {0}с", timer.Elapsed.TotalSeconds);
+        }
+
+        private async Task InitializeIdentityAsync()
+        {
+            _Logger.LogInformation("Инициализация БД системы Identity");
+            var timer = Stopwatch.StartNew();
+
+            async Task CheckRole(string RoleName)
+            {
+                if (!await _RoleManager.RoleExistsAsync(RoleName))
+                {
+                    _Logger.LogInformation("Роль {0} отсутствует. Создаю...", RoleName);
+                    await _RoleManager.CreateAsync(new Role { Name = RoleName });
+                    _Logger.LogInformation("Роль {0} создана успешно...", RoleName);
+                }
+            }
+
+            await CheckRole(Role.Administrators);
+            await CheckRole(Role.Users);
+
+            if(await _UserManager.FindByNameAsync(User.Administrator) is null)
+            {
+                _Logger.LogInformation("Пользователь {0} отсутствует. Создаю...", User.Administrator);
+
+                var admin = new User
+                {
+                    UserName = User.Administrator
+                };
+
+                var creation_result = await _UserManager.CreateAsync(admin, User.DefaultAdminPassord);
+                
+                if (creation_result.Succeeded)
+                {
+                    _Logger.LogInformation("Пользователь {0} успешно создан", User.Administrator);
+
+                    await _UserManager.AddToRoleAsync(admin, Role.Administrators);
+                    _Logger.LogInformation("Пользователь {0} наделен ролью {1}",
+                        User.Administrator, Role.Administrators);
+                }
+                else
+                {
+                    var errors = creation_result.Errors.Select(e => e.Description).ToArray();
+                    _Logger.LogError("Учётная запись администратора не создана по причине: {0}",
+                        string.Join(", ", errors));
+
+                    throw new InvalidOperationException($"Ошибка при создании пользователя {User.Administrator} : {string.Join(", ", errors)}");
+                }
+            }
+
+            _Logger.LogInformation("Инициализация данных БД системы Identity выполнена за {0}", timer.Elapsed.TotalSeconds);
         }
     }
 }
