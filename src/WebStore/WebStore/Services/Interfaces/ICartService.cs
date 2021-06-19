456fe81323cc89bff3c08a7ccb08a0f9b454be5b
but-sergey <but-sergey@bk.ru>
2021-06-19 11:29:00 +0300

    Заготовка сервиса Корзина

---
+++ src/WebStore/WebStore/Services/Interfaces/ICartService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.ViewModels;
+
+namespace WebStore.Services.Interfaces
+{
+    public interface ICartService
+    {
+        void Add(int Id);
+
+        void Decrement(int Id);
+
+        void Remove(int Id);
+
+        void Clear();
+
+        CartViewModel GetViewModel();
+    }
+}
