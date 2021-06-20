cb3c2810680ef77ec2b446b5e6c85b58a899621a
but-sergey <but-sergey@bk.ru>
2021-06-20 20:03:18 +0300

    Рефакторинг после переноса сотрудников

--- src/WebStore/WebStore/Controllers/EmployeesController.cs
+++ src/WebStore/WebStore/Controllers/EmployeesController.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.Entities;
 using WebStore.Domain.Entities.Identity;
-using WebStore.Models;
 using WebStore.Services.Interfaces;
 using WebStore.ViewModels;
 
