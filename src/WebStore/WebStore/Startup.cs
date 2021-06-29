8f3df32f9eccdd292ddc44fff1d3c1898638c39e
but-sergey <but-sergey@bk.ru>
2021-06-29 11:23:13 +0300

    Заготовка области администратора

--- src/WebStore/WebStore/Startup.cs
+++ src/WebStore/WebStore/Startup.cs
@@ -126,6 +126,11 @@ namespace WebStore
                 });
 
                 endpoints.MapControllerRoute(
+                    name: "areas",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
+
+                endpoints.MapControllerRoute(
                     "default",
                     "{controller=Home}/{action=Index}/{id?}");
             });
