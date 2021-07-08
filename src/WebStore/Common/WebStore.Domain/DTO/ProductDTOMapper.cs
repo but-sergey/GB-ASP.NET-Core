94bc569258bf44296cd778836f9e8b1eedcfc957
but-sergey <but-sergey@bk.ru>
2021-07-08 13:07:14 +0300

    Разнесение DTO-моделей и мапперов к ним по разным файлам

---
+++ src/WebStore/Common/WebStore.Domain/DTO/ProductDTOMapper.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using WebStore.Domain.Entities;
+
+namespace WebStore.Domain.DTO
+{
+    public static class ProductDTOMapper
+    {
+        public static ProductDTO ToDTO(this Product Product) => Product is null
+            ? null
+            : new ProductDTO
+            {
+                Id = Product.Id,
+                Name = Product.Name,
+                Order = Product.Order,
+                Price = Product.Price,
+                ImageUrl = Product.ImageUrl,
+                Brand = Product.Brand.ToDTO(),
+                Section = Product.Section.ToDTO(),
+            };
+
+        public static Product FromDTO(this ProductDTO Product) => Product is null
+            ? null
+            : new Product
+            {
+                Id = Product.Id,
+                Name = Product.Name,
+                Order = Product.Order,
+                Price = Product.Price,
+                ImageUrl = Product.ImageUrl,
+                Brand = Product.Brand.FromDTO(),
+                Section = Product.Section.FromDTO(),
+            };
+        public static IEnumerable<ProductDTO> ToDTO(this IEnumerable<Product> Products) => Products.Select(ToDTO);
+        public static IEnumerable<Product> FromDTO(this IEnumerable<ProductDTO> Products) => Products.Select(FromDTO);
+
+    }
+}
