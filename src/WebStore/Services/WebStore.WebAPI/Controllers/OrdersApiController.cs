41d317d9e0dc8a118d80cc1d639150a1b9e3c0cf
but-sergey <but-sergey@bk.ru>
2021-07-09 14:01:37 +0300

    Клиент и контроллер WebAPI сервиса заказов

---
+++ src/WebStore/Services/WebStore.WebAPI/Controllers/OrdersApiController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using WebStore.Domain.DTO.Orders;
+using WebStore.Interfaces;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.WebAPI.Controllers
+{
+    [Route(WebAPIAddress.Orders)]
+    [ApiController]
+    public class OrdersApiController : ControllerBase
+    {
+        private readonly IOrderService _OrderService;
+
+        public OrdersApiController(IOrderService OrderService) => _OrderService = OrderService;
+
+        [HttpGet("user/{UserName}")]
+        public async Task<IActionResult> GetUserOrders(string UserName)
+        {
+            var orders = await _OrderService.GetUserOrder(UserName);
+            return Ok(orders.ToDTO());
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetOrderById(int id)
+        {
+            var order = await _OrderService.GetOrderById(id);
+            return Ok(order.ToDTO());
+        }
+
+        [HttpPost("{UserName}")]
+        public async Task<IActionResult> CreateOrder(string UserName, [FromBody] CreateOrderDTO OrderModel)
+        {
+            var order = await _OrderService.CreateOrder(UserName, OrderModel.Items.ToCartView(), OrderModel.Order);
+            return Ok(order.ToDTO());
+        }
+    }
+}
