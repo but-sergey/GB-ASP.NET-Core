f2eb02d09c462584b1f776f53e8a7d26b9aa5269
but-sergey <but-sergey@bk.ru>
2021-07-10 18:58:25 +0300

    Заготовка контроллеров для работы с пользователями и ролями через WebAPI

---
+++ src/WebStore/Services/WebStore.WebAPI/Controllers/Identity/RolessApiController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.DAL.Context;
+using WebStore.Domain.Entities.Identity;
+using WebStore.Interfaces;
+
+namespace WebStore.WebAPI.Controllers.Identity
+{
+    [Route(WebAPIAddress.Identity.Roles)]
+    [ApiController]
+    public class RolessApiController : ControllerBase
+    {
+        private readonly RoleStore<Role> _RoleStore;
+
+        public RolessApiController(WebStoreDB db)
+        {
+            _RoleStore = new RoleStore<Role>(db);
+        }
+    }
+}
