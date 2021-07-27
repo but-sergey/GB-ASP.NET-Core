7a24448dd26c9e704426eb52d8762436178bfa74
but-sergey <but-sergey@bk.ru>
2021-07-27 16:16:02 +0300

    Основная логика таг-хелпера выделения активной ссылки

--- UI/WebStore/TagHelpers/ActiveRoute.cs
+++ UI/WebStore/TagHelpers/ActiveRoute.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace WebStore.TagHelpers
 {
@@ -12,7 +13,7 @@ namespace WebStore.TagHelpers
         private const string AttributeName = "ws-is-active-route";
 
         [HtmlAttributeName("asp-controller")]
-        public string Conroller { get; set; }
+        public string Controller { get; set; }
 
         [HtmlAttributeName("asp-action")]
         public string Action { get; set; }
@@ -33,14 +34,39 @@ namespace WebStore.TagHelpers
 
         private bool IsActive()
         {
+            var route_values = ViewContext.RouteData.Values;
 
+            var current_controller = route_values["controller"]?.ToString();
+            var current_action = route_values["action"]?.ToString();
+
+            const StringComparison str_cmp = StringComparison.OrdinalIgnoreCase;
+
+            if (!string.IsNullOrEmpty(Controller) && !string.Equals(Controller, current_controller, str_cmp))
+                return false;
+
+            if (!string.IsNullOrEmpty(Action) && !string.Equals(Action, current_action, str_cmp))
+                return false;
+
+            foreach (var (key, value) in RouteValues)
+                if (!route_values.ContainsKey(key) || route_values[key]?.ToString() != value)
+                    return false;
 
             return true;
         }
 
         private static void MakeActive(TagHelperOutput output)
         {
+            var class_attribute = output.Attributes.FirstOrDefault(attr => attr.Name == "class");
 
+            if (class_attribute is null)
+                output.Attributes.Add("class", "active");
+            else
+            {
+                if (class_attribute.Value.ToString()?.Contains("active") ?? false)
+                    return;
+
+                output.Attributes.SetAttribute("class", class_attribute.Value + " active");
+            }
         }
     }
 }
