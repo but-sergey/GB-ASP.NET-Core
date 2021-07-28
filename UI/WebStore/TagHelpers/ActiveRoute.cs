bf42840964fcb6bfffb52e0ec10fb25c03e45f01
but-sergey <but-sergey@bk.ru>
2021-07-28 08:52:34 +0300

    Обеспециваем возможность отключения проверки совпадения имени действия для активации ссылки

--- UI/WebStore/TagHelpers/ActiveRoute.cs
+++ UI/WebStore/TagHelpers/ActiveRoute.cs
@@ -11,6 +11,7 @@ namespace WebStore.TagHelpers
     public class ActiveRoute : TagHelper
     {
         private const string AttributeName = "ws-is-active-route";
+        private const string IgnoreAction = "ws-ignore-action";
 
         [HtmlAttributeName("asp-controller")]
         public string Controller { get; set; }
@@ -26,13 +27,15 @@ namespace WebStore.TagHelpers
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
-            if (IsActive())
+            var is_ignore_Action = output.Attributes.ContainsName(IgnoreAction);
+
+            if (IsActive(is_ignore_Action))
                 MakeActive(output);
 
             output.Attributes.RemoveAll(AttributeName);
         }
 
-        private bool IsActive()
+        private bool IsActive(bool IsIgnoreAction)
         {
             var route_values = ViewContext.RouteData.Values;
 
@@ -41,10 +44,12 @@ namespace WebStore.TagHelpers
 
             const StringComparison str_cmp = StringComparison.OrdinalIgnoreCase;
 
-            if (!string.IsNullOrEmpty(Controller) && !string.Equals(Controller, current_controller, str_cmp))
+            if (Controller is { Length: > 0 } controller && !string.Equals(controller, current_controller, str_cmp))
                 return false;
 
-            if (!string.IsNullOrEmpty(Action) && !string.Equals(Action, current_action, str_cmp))
+            if (IsIgnoreAction) return true;
+
+            if (Action is { Length: > 0 } action && !string.Equals(action, current_action, str_cmp))
                 return false;
 
             foreach (var (key, value) in RouteValues)
